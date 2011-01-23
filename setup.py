5a494732da2aef865e62dc7b09551fa90a90b700
Zachary Goldberg <zgoldberg@src.gnome.org>
2011-01-23 13:24:14 -0500

    work on building a seperate egg for the client

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 from setuptools import setup
 
+
 setup(
     name = "Personal Cluster Management",
     version = "0.0.1",
@@ -26,6 +27,30 @@ setup(
         },
     
 )
+
+setup(
+    name = "Personal Cluster Management Client",
+    version = "0.0.1",
+    author = "Zachary Goldberg",
+    author_email = "zach@zachgoldberg.com",
+    description = ("Tools and programs to manage your own unix machines"),
+    license = "GPL",
+    keywords = "ssh management cluster personal openssh",
+    url = "http://packages.python.org/unix_pcm",
+    packages=['pcm', 'pcm/common'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GPL License",
+    ],
+    entry_points = {
+        "console_scripts": [
+            "pcm_client = pcm.client:run",
+            ],
+        },
+    
+)
+
 
 os.system("cp etc/pcm_client /etc/init.d")
 os.system("chmod o+x /etc/init.d/pcm_client")
